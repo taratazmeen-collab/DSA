3e4b947f8324f08a929eb45c26735f8669ff7c2f
taratazmeen-collab <taratazmeen@icloud.com>
2026-01-03 23:42:27 +0530

    Add files via upload

---
+++ WEEK -1/question 1 .py
@@ -0,0 +1,13 @@
+# Different data types in python
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+student = bool(input("Are you a student? (true/false): "))
+
+#printing the details
+
+print("Name : " , name)
+print("Age : " ,age)
+print("height : ",height)
+print("Are you a student : " ,student)
