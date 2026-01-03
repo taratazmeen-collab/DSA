9828fedb9a0a559dd7b6cc3933b7392d168e5ea8
taratazmeen-collab <taratazmeen@icloud.com>
2026-01-03 23:54:36 +0530

    Add files via upload

---
+++ WEEK -1/question8.py
@@ -0,0 +1,10 @@
+signal = input("Enter traffic light colour: ")
+
+if signal == "red":
+    print("STOP!")
+elif signal == "yellow":
+    print("Get ready to start")
+elif signal == "green":
+    print("You can go")
+else:
+    print("Wrong input!")
