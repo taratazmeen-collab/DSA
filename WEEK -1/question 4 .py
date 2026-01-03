a3373cf48b70feec9f3abe009fb9db590248e724
taratazmeen-collab <taratazmeen@icloud.com>
2026-01-03 23:48:44 +0530

    Add files via upload

---
+++ WEEK -1/question 4 .py
@@ -0,0 +1,6 @@
+# creating with variable with the users fav number
+fav=int(input("Enter your fav number : "))
+if(fav%2==0):
+        print("It is an even number")
+else:
+        print("it is a odd number")
